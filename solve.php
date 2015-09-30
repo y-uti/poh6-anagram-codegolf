45beb8a1f7684f9c6845bed82575ce18536a32d0
y-uti <y-uti@users.noreply.github.com>
2015-09-30 23:54:11 +0900

    $r への代入を配列アクセスに統合。論理演算子を 1 文字の演算子に置き換え

--- solve.php
+++ solve.php
@@ -1,8 +1,7 @@
 <?php
 while ($w = chop(fgets(STDIN))) {
-    $r = strrev($w);
-    if ($buffer[$r] && $buffer[$r]-- && ($pairs[] = min($w, $r))
-        || ++$buffer[$w] && $r == $w && $w < 1 && $centers[$w] = 0) {
+    if ($buffer[$r = strrev($w)] && $buffer[$r]-- . ($pairs[] = min($w, $r))
+        || ++$buffer[$w] & $r == $w & $w < 1 && $centers[$w] = 0) {
         unset($centers[$w]);
     }
 }
